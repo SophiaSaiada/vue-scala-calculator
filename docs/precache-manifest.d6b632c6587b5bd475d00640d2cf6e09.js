8293426b50af37bf0720fe47b2804d0118799124
Yossi <yossi@feezback.com>
2018-10-30 21:59:04 +0200

    add demo

---
+++ docs/precache-manifest.d6b632c6587b5bd475d00640d2cf6e09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "7533f4c5be79355b5408",
+    "url": "/js/chunk-vendors.f6d1836c.js"
+  },
+  {
+    "revision": "8369ba3f8645f94373e3",
+    "url": "/js/app.ae3e4f3f.js"
+  },
+  {
+    "revision": "434c75cada7bda62b5054a9efad69c3a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "704813153799119a940754890700f053",
+    "url": "/img/baseline-arrow_back-24px.70481315.svg"
+  },
+  {
+    "revision": "8369ba3f8645f94373e3",
+    "url": "/css/app.920c9134.css"
+  }
+];
